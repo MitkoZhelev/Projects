f929448c614913ffec00a2766bf6def8d01a5a8e
Mechkostuk <Mechkostuk@gmail.com>
2017-11-29 22:49:39 +0200

    tes

---
+++ Documents/Visual Studio 2015/Projects/Arrays3/Arrays3/IntroArrays.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays3
+{
+    class IntroArrays
+    {
+
+        public static void Main()
+        {
+
+
+            //polynoms
+
+            string str = "(3x2 + x2 - 3)";
+
+            int powCounter = 0;
+            int [] arrPow = new int[10];
+            if (str.Contains("x"))
+            {
+                char[] charArr = str.ToCharArray();
+
+                for (int i = 0; i < charArr.Length; i++)
+                {
+                    if ( (++i < charArr.Length - 1) && (charArr[i] == 'x' || Char.IsDigit(charArr[i++])))
+                    {
+                        int pow = charArr[i++] & 0x0f;//(int)Char.GetNumericValue(charArr[i++]);
+                        Console.WriteLine(charArr[i++]);
+                        arrPow[powCounter] = pow;
+                    }
+                }
+            }
+
+            foreach (var item in arrPow)
+            {
+               // Console.WriteLine(item);
+            }
+            Console.WriteLine("After");
+            //multidimensional arrays
+
+
+            string[,] arr = new string[5, 3];
+
+            //jagged array
+
+            int[][] jaggedArr = new int[5][];
+
+            int row = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[row, col];
+            int inc = 0;
+
+            //matrix.GetLength(0) - rows
+            //matrix.GetLength(1) - cols
+
+            int counter = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    matrix[i, j] = ++counter;
+                 
+                }
+            }
+
+            int count = 1;
+            for (int i = 0; i < matrix.GetLength(0); i++) 
+            {
+
+                /****
+                ****
+                0,0| 1,3 | 2,0| 3,3
+                */
+
+               
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                           
+                            
+                          Console.Write(matrix[j, i]);
+                    count++;
+                    if (count % 2 == 0)
+                    {
+                            for (int p = matrix.GetLength(1) - 1; p > 0; p--)
+                            {
+
+
+                                Console.Write(matrix[i++, p]);
+
+                            }
+
+                        }
+
+                }
+              
+                  
+               
+                Console.WriteLine();
+               
+            }
+
+        }
+
+
+
+
+        private static int[] printArr(int row, int inc)
+        {
+            int increment = inc;
+            int[] arr = new int[row];
+            for (int i = 0; i < row; i++)
+            {
+
+                // Console.WriteLine(++increment);
+                arr[i] = increment;
+            }
+            return arr;
+        }
+
+
+        private static int[] reverseArr(int row, int inc)
+        {
+            int increment = inc;
+            int[] arr = new int[row];
+            for (int i = row - 1; i >= 0; i--)
+            {
+                //Console.WriteLine(increment);
+                arr[i] = increment;
+                --increment;
+            }
+
+            return arr;
+        }
+
+    }
+}
---
+++ Documents/Visual Studio 2015/Projects/Arrays3/Arrays3/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+namespace Arrays3
+{
+
+   class Program
+   {
+
+       static void Main(string[] args)
+       {
+           int row;
+           int col;
+           ArrayList arr = new ArrayList();
+
+
+
+           Console.WriteLine("Enter how many elements");
+           row = int.Parse(Console.ReadLine());
+           col = row;
+           int arrMax = row; 
+           int[,] arr1 = new int[row, col];
+
+           int[] rowPrint = new int[row*row];
+           int[] colPrint = new int[row*row];
+
+           //for (int i1 = 0; i1 < row; i1++)
+           //{
+           //    for (int i2 = 0; i2 < col; i2++)
+           //    {
+           //        arr1[i1, i2] = int.Parse(Console.ReadLine());
+           //    }
+           //}
+
+           int[][] arr2= new int[3][3];
+
+           int inc = 0;
+           int number = ((row * row) / 2) - 1;
+           Console.WriteLine("Number is :" + number);
+
+           for (int i = 1; i <= row; i++)
+           {
+
+
+               if (i%2 == 0 )
+                {
+                   inc += row;
+                   rowPrint = reverseArr(row, inc);
+                 }
+                   else
+                 {
+
+                   colPrint =  printArr(row, inc);
+                   inc += row;
+               }
+
+
+           }
+
+           for (int i = 0; i < row*2; i++)
+           {
+               Console.WriteLine(rowPrint[i]);
+           }
+
+           int test = 0;
+          // int[] numbers = arr.ToArray(typeof(int)) as int[];
+
+           int[] res = arr.OfType<int>().ToArray();
+           Console.WriteLine(res.Length);
+           foreach (var item in res)
+           {
+              // Console.WriteLine(item);
+               Console.WriteLine(item);
+           }
+
+           //for (int i1 = 0; i1 < col; i1++)
+           //{
+           //    for (int i2 = 0; i2 < row; i2++)
+           //    {
+           //        arr1[i1, i2] = int.Parse(Console.ReadLine());
+           //        if (i1 == row  )
+           //        {
+           //            for (int i = row; i <= 0; i--)
+           //            {
+
+           //            }
+           //        }
+           //    }
+           //}
+
+       }
+
+       private static int [] reverseArr(int row, int inc)
+       {
+           int increment = inc;
+           int[] arr = new int[row];
+           for (int i = row - 1; i >= 0; i--)
+           {
+               //Console.WriteLine(increment);
+               arr[i] = increment;
+               --increment;
+           }
+
+           return arr;
+       }
+
+       private static int[] printArr(int row, int inc)
+       {
+           int increment = inc;
+           int[] arr = new int[row];
+           for (int i = 0; i < row; i++)
+           {
+
+              // Console.WriteLine(++increment);
+               arr[i] = increment;
+           }
+           return arr;
+       }
+   }
+
+}
+*/

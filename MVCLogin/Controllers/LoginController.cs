ecef855fdbb390927c2791e724cbf1e93fcfe863
MitkoZhelev <mitko.j.zhelev@gmail.com>
2018-01-14 00:31:52 +0200

    MVC Login

---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using MVCLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(MVCLogin.Models.User userModel)
+        {
+            using (LoginDataBaseEntities db = new LoginDataBaseEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UsernId;
+                    Session["userName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+       
+    }
+}
